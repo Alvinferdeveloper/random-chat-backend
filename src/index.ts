6bf4d5000d014f20759e05765240a4ed5e7a2fff
Albin <alvinfer67@gmail.com>
2025-08-21 16:23:43 -0600

    feat: set cors and better auth trusted origins

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,14 @@ import { Server } from 'socket.io';
 import { handleSocketEvents } from './controllers/messageController';
 import { ChatService } from './services/chat.service';
 import roomRouter from './routes/v1/room.route';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
+import cors from 'cors';
 
 const app = express();
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  next();
-})
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
   maxHttpBufferSize: 1e7,
 });
 
@@ -24,7 +20,14 @@ const chatService = new ChatService(io);
 io.on('connection', (socket) => {
   handleSocketEvents(socket, chatService);
 });
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use('/api/v1/room', roomRouter);
 
 const PORT = process.env.PORT || 3001;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,4 +6,12 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "mysql",
     }),
+    socialProviders: {
+        google: {
+            prompt: "select_account",
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
+    trustedOrigins: ["http://localhost:3000"],
 });
